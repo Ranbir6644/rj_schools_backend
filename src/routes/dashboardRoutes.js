643c1224b121164fb006a5d42a4b0006debb9cbe
Ranbir6644 <ranbir6644@gmail.com>
2026-09-20 20:37:05 +0530

    Update dashboardRoutes.js

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -26,30 +26,3 @@ router.get("/record", getDashboardRecord);                     // Attendance + f
 router.get("/complete", getCompleteDashboard);                 // All dashboard data in one call
 
 export default router;
-// import express from "express";
-// import {
-//   getDashboardSummary,
-//   getAttendanceGraphData,
-//   getGenderDistribution,
-//   getAllClasses,
-//   getCompleteDashboard,
-//   getDailyAttendanceGraphData
-// } from "../controllers/dashboardController.js";
-// import { protect } from "../middleware/authMiddleware.js";
-
-// const router = express.Router();
-
-// // All routes require authentication
-// router.use(protect);
-
-// // Dashboard routes
-// router.get("/summary", getDashboardSummary);                    // Top 4 cards data
-// router.get("/attendance-graph", getAttendanceGraphData);       // Graph data with filters
-// router.get("/daily-attendance", getDailyAttendanceGraphData);  //Newly added on 29-10-2025 Daily graph data - Datewise
-
-// router.get("/gender-distribution", getGenderDistribution);     // Pie chart data
-// router.get("/classes", getAllClasses);                         // All classes for dropdown
-// router.get("/complete", getCompleteDashboard);                 // All dashboard data in one call
-
-
-// export default router;
